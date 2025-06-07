03bb1a55e55362f677909b52a5ab1490771a3648
Jakob Eriksson <jkberiksson@gmail.com>
2025-06-07 17:56:31 +0200

    Update confirmation text in Step4 components for Glas and Nyckel pages to clarify the submission process and ensure consistency in messaging regarding insurance coverage and payment obligations.

--- app/(public)/ny-skada/glas/[id]/components/Step4.jsx
+++ app/(public)/ny-skada/glas/[id]/components/Step4.jsx
@@ -131,9 +131,9 @@ export default function Step4({ register, errors, signatureRef, formData, signat
             <div className='space-y-6 bg-accent-red/5 rounded-xl p-6 shadow my-6'>
                 <h3 className='text-lg font-bold text-text-primary'>Bekräftelse</h3>
                 <p className='text-sm text-text-primary'>
-                    Härmed intygas riktigheten av ovanstående uppgifter samt att försäkringen omfattar glasruteskada och att premien var
-                    betald vid skadetillfället. Godtar försäkringsbolaget inte skadan som försäkringsgrundande är fordonsägaren alltid
-                    betalningsskyldig.
+                    Genom att klicka på knappen "Skicka anmälan" intygas riktigheten av ovanstående uppgifter samt att försäkringen omfattar
+                    glasruteskada och att premien var betald vid skadetillfället. Godtar försäkringsbolaget inte skadan som
+                    försäkringsgrundande är fordonsägaren alltid betalningsskyldig.
                 </p>
             </div>
         </>
